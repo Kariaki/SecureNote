d738fe71f1038d1d121eb5131db4a0566c8ab21d
Kariaki <kariaki@Digicores-MacBook-Pro-2.local>
2023-09-26 11:13:16 +0100

    initialized and setup project

---
+++ app/src/main/java/com/getontop/candidate/kariaki/assessment/core/AssessmentApplication.kt
@@ -0,0 +1,7 @@
+package com.getontop.candidate.kariaki.assessment.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AssessmentApplication: Application()
